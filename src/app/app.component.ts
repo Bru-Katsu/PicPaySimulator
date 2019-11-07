c78ad80d42185494600e2265ca438227ba398001
Bruno Alves <ninjaextremegfx@gmail.com>
2019-11-07 00:32:23 -0300

    Melhorias na interface e validações reativas
    Adicionado Reactive Forms a aplicação e feito algumas melhorias no visual da aplicação.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -11,40 +12,59 @@ export class AppComponent implements OnInit {
   private readonly _JurosCartao: number = 0
   private readonly _JurosParcela: number = 0
   public readonly _Parcelas: number[] = new Array()
-  public _SelectedValue: number = 0
-  public _Valor: number = 0
+  public _ValorSelecionado: number = 1
+  public _Saldo: number
+  public _bHasZeroSaldo: boolean = false
+  public _Valor: number
   public _Total: number = 0
   public _ValorJurosCartao: number = 0
   public _ValorJurosParcela: number = 0
   public _TotalParcelas: number = 0
+  public _Form: FormGroup
 
   ngOnInit(){
+    this.validation()
   }
   
-  constructor() {
+  constructor(private _FormBuilder: FormBuilder) {
     for(let i = 1; i < 13; i++)
     this._Parcelas.push(i)
     this._JurosCartao = 2.99
     this._JurosParcela = 3.49
   }
 
-  private ClearValues(): void{
-    this._ValorJurosCartao = 0
-    this._ValorJurosParcela = 0
-    this._Total = 0
+  private validation(){
+    this._Form = this._FormBuilder.group({
+      saldo: ['', [Validators.min(0.01)]],
+      valor:['', [Validators.required, Validators.min(0.01), Validators.max(5000)]],
+      parcelas: [1, [Validators.required, Validators.min(1)]]
+    })
   }
 
-  public CalcTabelaPrice(valor: number, juros: number, meses: number): number{
+  private CalcTabelaPrice(valor: number, juros: number, meses: number): number{
     juros = juros / 100
     let parecela = valor * juros / (1 - 1 / (1 + juros) ** meses)
     return parecela * meses
   }
 
-  public Calcular(): void{
-    let valorboleto = this.CalcTabelaPrice(this._Valor, this._JurosCartao, 1)
-    let parecela = this.CalcTabelaPrice(valorboleto, this._JurosParcela, parseInt(this._SelectedValue.toString()))
+  private InitValues(): void{
+    this._bHasZeroSaldo = this._Saldo == undefined ? true : false
+    if(this._bHasZeroSaldo)
+      this._Saldo = 0
+  }
 
-    this._Total = parecela 
-    this._TotalParcelas = parecela / this._SelectedValue
+  private DefaultValues(): void{
+    this._Saldo = undefined
+    this._bHasZeroSaldo = false
+  }
+
+  public Calcular(): void{
+    this.InitValues()
+    let valorboleto = this.CalcTabelaPrice(this._Valor - this._Saldo, this._JurosCartao, 1)
+    let parcela = this.CalcTabelaPrice(valorboleto, this._JurosParcela, parseInt(this._ValorSelecionado.toString()))
+
+    this._Total = parcela 
+    this._TotalParcelas = parcela / this._ValorSelecionado
+    this.DefaultValues()
   }
 }
